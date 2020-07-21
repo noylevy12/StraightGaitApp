9834509c6328babb96f278cfd5d0ca141d52462a
noylevy12 <50630415+noylevy12@users.noreply.github.com>
2020-07-21 22:18:03 +0300

    Revert "add Device exe file"
    This reverts commit c160cdc7e6dde4dd749c408f613822cb9e8dea07.

--- deviceSimulate/deviceWindow.java
+++ deviceSimulate/deviceWindow.java
@@ -13,15 +13,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Enumeration;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.awt.event.ActionEvent;
@@ -53,19 +47,14 @@ public class deviceWindow {
 	Random rand= new Random();  // this random number simulate the degree of the leg
 	RotatedIcon rotatedIcon;
 	int angle =0;
-	final int MAX_ANGLE = 42;
-	final int MIN_ANGLE = 0;
-	private static String currentHostIpAddress;
+
 
 
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args)   {
-
-		
-				
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -76,8 +65,6 @@ public class deviceWindow {
 				}
 			}
 		});
-		currentHostIpAddress= getCurrentEnvironmentNetworkIp();
-		lblConnectionStatus.setText("for connecting to this device, enter the following IP address:\n"+ currentHostIpAddress);
 
 		try {
 			serverSocket = new ServerSocket(7800);
@@ -87,8 +74,8 @@ public class deviceWindow {
 				bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				ipAddress=bufferedReader.readLine();
 				if(ipAddress != null) {
-					System.out.println("connected to app in IP address: "+ipAddress);
-					lblConnectionStatus.setText("connected to app in IP address: "+ ipAddress);
+					System.out.println("connected to IP address: "+ipAddress);
+					lblConnectionStatus.setText("connected to IP address: "+ ipAddress);
 					btnMoveLeg_left.setEnabled(true);
 					btnMoveLeg_right.setEnabled(true);
 
@@ -105,8 +92,6 @@ public class deviceWindow {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 
 	/**
@@ -122,7 +107,7 @@ public class deviceWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 500, 400);
+		frame.setBounds(100, 100, 452, 376);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -131,12 +116,12 @@ public class deviceWindow {
 		btnMoveLeg_left.setIcon(new ImageIcon(deviceWindow.class.getResource("/com/sun/javafx/scene/web/skin/Undo_16x16_JFX.png")));
 		btnMoveLeg_left.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnMoveLeg_left.setBackground(new Color(152, 251, 152));
-		btnMoveLeg_left.setBounds(40, 274, 186, 30);
+		btnMoveLeg_left.setBounds(27, 274, 186, 30);
 		btnMoveLeg_left.setEnabled(false);
 		btnMoveLeg_left.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Obtain a degree number between [0 - 33].					
-				angle = ThreadLocalRandom.current().nextInt(MIN_ANGLE, angle+1);
+				angle = ThreadLocalRandom.current().nextInt(0, angle+1);
 				System.out.println(angle);
 				System.out.println(ipAddress);
 				lblAngle.setText("Sample angle: "+ String.valueOf(angle) );
@@ -144,7 +129,7 @@ public class deviceWindow {
 				lblLegImg.setIcon(rotatedIcon);
 				System.out.println("writting to android");
 				if(angle != 0) {
-					if(angle != MAX_ANGLE) {
+					if(angle != 32) {
 						btnMoveLeg_right.setEnabled(true);
 					}
 					try {
@@ -176,18 +161,18 @@ public class deviceWindow {
 		btnMoveLeg_right.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnMoveLeg_right.setEnabled(false);
 		btnMoveLeg_right.setBackground(new Color(152, 251, 152));
-		btnMoveLeg_right.setBounds(260, 274, 186, 30);
+		btnMoveLeg_right.setBounds(221, 274, 186, 30);
 		btnMoveLeg_right.setIcon(new ImageIcon(deviceWindow.class.getResource("/com/sun/javafx/scene/web/skin/Redo_16x16_JFX.png")));
 		btnMoveLeg_right.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				angle = ThreadLocalRandom.current().nextInt(angle, MAX_ANGLE+1);
+				angle = ThreadLocalRandom.current().nextInt(angle, 42);
 				System.out.println(angle);
 				System.out.println(ipAddress);
 				lblAngle.setText("Sample angle: "+ String.valueOf(angle) );
 				rotatedIcon = new RotatedIcon(imgIcon, angle);
 				lblLegImg.setIcon(rotatedIcon);
 				System.out.println("writting to android");
-				if(angle != MAX_ANGLE) {
+				if(angle != 42) {
 					if(angle != 0) {
 						btnMoveLeg_left.setEnabled(true);
 					}
@@ -224,17 +209,16 @@ public class deviceWindow {
 
 		JLabel lblHeader = new JLabel("Straight Gait Device simulator");
 		lblHeader.setForeground(Color.WHITE);
-		lblHeader.setBounds(0, 15, 500, 25);
+		lblHeader.setBounds(0, 15, 434, 17);
 		lblHeader.setBackground(Color.WHITE);
 		lblHeader.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblHeader.setHorizontalAlignment(SwingConstants.CENTER);
 		frame.getContentPane().add(lblHeader);
 
 		lblConnectionStatus = new JLabel("connection status");
-		lblConnectionStatus.setForeground(Color.WHITE);
 		lblConnectionStatus.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblConnectionStatus.setHorizontalAlignment(SwingConstants.CENTER);
-		lblConnectionStatus.setBounds(0, 43, 500, 14);
+		lblConnectionStatus.setBounds(0, 43, 432, 14);
 		lblConnectionStatus.setText("There is no connection");
 		frame.getContentPane().add(lblConnectionStatus);
 
@@ -243,7 +227,7 @@ public class deviceWindow {
 		Image img = new ImageIcon(this.getClass().getResource("/foot.png")).getImage();
 		imgIcon = new ImageIcon(this.getClass().getResource("/foot.png"));
 		lblLegImg.setIcon(imgIcon);
-		lblLegImg.setBounds(170, 68, 213, 184);		
+		lblLegImg.setBounds(120, 68, 213, 184);		
 		frame.getContentPane().add(lblLegImg);
 		
 		lblAngle = new JLabel("");
@@ -256,40 +240,15 @@ public class deviceWindow {
 		Image backgroundImg = new ImageIcon(this.getClass().getResource("/background.PNG")).getImage();
 		backgroundImgIcon = new ImageIcon(this.getClass().getResource("/background.PNG"));
 		lblLegBackgroundImg.setIcon(backgroundImgIcon);
-		lblLegBackgroundImg.setBounds(-12, -81, 500, 500);		
+		lblLegBackgroundImg.setBounds(-12, -81, 448, 418);		
 		frame.getContentPane().add(lblLegBackgroundImg);
+
+
+
+
+
 	}
 	
 
-    public static String getCurrentEnvironmentNetworkIp() {
-        if (currentHostIpAddress == null) {
-            Enumeration<NetworkInterface> netInterfaces = null;
-            try {
-                netInterfaces = NetworkInterface.getNetworkInterfaces();
-
-                while (netInterfaces.hasMoreElements()) {
-                    NetworkInterface ni = netInterfaces.nextElement();
-                    Enumeration<InetAddress> address = ni.getInetAddresses();
-                    while (address.hasMoreElements()) {
-                        InetAddress addr = address.nextElement();
-  //                      log.debug("Inetaddress:" + addr.getHostAddress() + " loop? " + addr.isLoopbackAddress() + " local? "
-    //                            + addr.isSiteLocalAddress());
-                        if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()
-                                && !(addr.getHostAddress().indexOf(":") > -1)) {
-                            currentHostIpAddress = addr.getHostAddress();
-                        }
-                    }
-                }
-                if (currentHostIpAddress == null) {
-                    currentHostIpAddress = "127.0.0.1";
-                }
-
-            } catch (SocketException e) {
-//                log.error("Somehow we have a socket error acquiring the host IP... Using loopback instead...");
-                currentHostIpAddress = "127.0.0.1";
-            }
-        }
-        return currentHostIpAddress;
-    }
 
 }
